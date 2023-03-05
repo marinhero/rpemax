f1c93e30637c8d12e72bdda2eaa871817439ff07
Marín Alcaraz <alcara_m@pm.me>
2023-03-04 17:22:45 -0800

    Initial implementation and tests

---
+++ index.ts
@@ -0,0 +1,51 @@
+// deno-fmt-ignore
+const RPE_TABLE: { [key: string]: Array<number> } = {
+  '10':     [100, 96, 92, 89, 86, 84, 81, 79, 76, 74],
+  '9.5':    [98, 94, 91, 88, 85, 82, 80, 77, 75, 72],
+  '9':      [96, 92, 89, 86, 84, 81, 79, 76, 74, 71],
+  '8.5':    [94, 91, 88, 85, 82, 80, 77, 75, 72, 69],
+  '8':      [92, 89, 86, 84, 81, 79, 76, 74, 71, 68],
+  '7.5':    [91, 88, 85, 82, 80, 77, 75, 72, 69, 67],
+  '7':      [89, 86, 84, 81, 79, 76, 74, 71, 68, 65],
+  '6.5':    [88, 85, 82, 80, 77, 75, 72, 69, 67, 64],
+}
+
+const TABLE_OFFSET = 1
+
+// 1. Look up Rep/RPE combo in the RPE chart and find the percentage.
+// 2. Divide the weight you used by the percentage you got from step one. This is your estimated one rep max.
+// 3. Multiply your one rep max for the percentage of it that you wish to lift.
+
+export const getOneRepMax = (
+  oneRepMaxPercentage: number,
+  weight: number,
+): number => {
+  return weight / oneRepMaxPercentage
+}
+
+export const getOneRepMaxPercentage = (rpe: number, reps: number): number => {
+  const rpeKey = rpe.toString()
+  const validRPEKeys = Object.keys(RPE_TABLE)
+  if (!validRPEKeys.includes(rpeKey)) {
+    throw Error('Unsopported RPE value. Supported values are ' + validRPEKeys)
+  }
+
+  if (reps > 10) {
+    throw Error('Rep number must be < 10')
+  }
+
+  return RPE_TABLE[rpeKey][reps - TABLE_OFFSET]
+}
+
+export const calculateWeight = (
+  rpe: number,
+  reps: number,
+  weight: number,
+  projectedWeightPercentage: number,
+): number => {
+  const oneRepMaxPercentage = getOneRepMaxPercentage(rpe, reps)
+  const oneRepMax = getOneRepMax(oneRepMaxPercentage, weight)
+  return Number((projectedWeightPercentage * oneRepMax).toFixed(2))
+}
+
+console.log(calculateWeight(8, 4, 155, 75))
---
+++ index_test.ts
@@ -0,0 +1,45 @@
+import {
+  assertEquals,
+  assertFalse,
+  assertInstanceOf,
+  assertIsError,
+  assertThrows,
+} from 'https://deno.land/std@0.178.0/testing/asserts.ts'
+import {
+  calculateWeight,
+  getOneRepMax,
+  getOneRepMaxPercentage,
+} from './index.ts'
+
+Deno.test('getOneRepMaxPercentage -- throws when RPE Key is invalid', () => {
+  assertThrows(() => {
+    getOneRepMaxPercentage(5, 5)
+  })
+})
+
+Deno.test('getOneRepMaxPercentage -- throws when rep count is invalid', () => {
+  assertThrows(() => {
+    getOneRepMaxPercentage(5, 15)
+  })
+})
+
+Deno.test('getOneRepMaxPercentage -- returns valid RPE percentage', () => {
+  assertEquals(getOneRepMaxPercentage(10, 1), 100)
+  assertEquals(getOneRepMaxPercentage(6.5, 10), 64)
+  assertEquals(getOneRepMaxPercentage(10, 5), 86)
+})
+
+Deno.test('getOneRepMax -- returns a valid one rep max estimate', () => {
+  assertEquals(getOneRepMax(getOneRepMaxPercentage(10, 1), 100), 1)
+  assertEquals(getOneRepMax(getOneRepMaxPercentage(6.5, 10), 100), 1.5625)
+  assertEquals(
+    getOneRepMax(getOneRepMaxPercentage(10, 5), 100),
+    1.1627906976744187,
+  )
+})
+
+Deno.test('calculateWeight', () => {
+  assertEquals(calculateWeight(10, 1, 100, 100), 100)
+  assertEquals(calculateWeight(8, 4, 155, 75), 138.39)
+  assertEquals(calculateWeight(7, 7, 135, 80), 145.95)
+})
